02bde9ed2e916bba3f7fe6c4b723db2b2fd388fe
unknown <Robert.Aron.tx@gmail.com>
2021-03-11 17:16:07 -0600

    make form static

--- pages/contact.js
+++ pages/contact.js
@@ -2,13 +2,13 @@ import {useState} from 'react'
 
 
 export default function Contact(){
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
     return (
         <div>
             <h1>hello world!!</h1>
-            {count}
+            {/* {count}
             <button onClick={()=>setCount(count+1)}>add</button>
-            <button onClick={()=>setCount(count-1)}>subtact</button>
+            <button onClick={()=>setCount(count-1)}>subtact</button> */}
             <form name='contact' action='/sucess' method='POST' data-netlify-recaptcha="true" data-netlify='true'>
                 <input type='hidden' name='form-name' value='contact'/>
                 <p>
